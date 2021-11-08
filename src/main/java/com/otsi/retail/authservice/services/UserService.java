e0e75195f1ec5142cf135160be10cf242397731e
manideepthaninki <78657731+manideepthaninki@users.noreply.github.com>
2021-11-08 22:02:02 +0530

    Added userService file

---
+++ src/main/java/com/otsi/retail/authservice/services/UserService.java
@@ -0,0 +1,25 @@
+package com.otsi.retail.authservice.services;
+
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import com.otsi.retail.authservice.Entity.UserDeatils;
+import com.otsi.retail.authservice.requestModel.GetUserRequestModel;
+
+@Component
+public interface UserService {
+/**
+ * 
+ * @param userRequest
+ * @return
+ * @throws Exception
+ */
+	List<UserDeatils> getUserFromDb(GetUserRequestModel userRequest) throws Exception;
+
+	List<UserDeatils> getUserForClient(int clientId) throws Exception;
+
+	List<UserDeatils> getUsersForClientDomain(long clientDomianId);
+	
+	
+}
