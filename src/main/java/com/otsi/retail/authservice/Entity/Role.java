42bc4703adf58ea8a5ac08cf3318c892269145f0
lakshmi <lakshmi.dasari@otsi.co.in>
2022-05-20 16:40:29 +0530

    added user childprivilages
    # Conflicts:
    #	pom.xml
    #	src/main/java/com/otsi/retail/authservice/Entity/ParentPrivilege.java
    #	src/main/java/com/otsi/retail/authservice/Entity/SubPrivilege.java
    #	src/main/java/com/otsi/retail/authservice/controller/RolesAndPrivillagesController.java
    #	src/main/java/com/otsi/retail/authservice/requestModel/CreateRoleRequest.java
    #	src/main/java/com/otsi/retail/authservice/requestModel/ParentPrivilegeVO.java
    #	src/main/java/com/otsi/retail/authservice/requestModel/SubPrivilegeVO.java
    #	src/main/java/com/otsi/retail/authservice/requestModel/SubPrivilegesVO.java
    #	src/main/java/com/otsi/retail/authservice/services/RolesAndPrivillagesService.java
    #	src/main/java/com/otsi/retail/authservice/services/RolesAndPrivillagesServiceImpl.java

--- src/main/java/com/otsi/retail/authservice/Entity/Role.java
+++ src/main/java/com/otsi/retail/authservice/Entity/Role.java
@@ -64,6 +64,12 @@ public class Role  extends BaseEntity implements Serializable  {
 	inverseJoinColumns = { @JoinColumn(name  = "id")})
 	private List<ChildPrivilege> childPrivilages;
 	
+	@ManyToMany(fetch = FetchType.LAZY,cascade =CascadeType.ALL)
+	@JoinTable(name = "role_childPrivilages",
+	joinColumns= { @JoinColumn(name = "roleId")},
+	inverseJoinColumns = { @JoinColumn(name  = "id")})
+	private List<ChildPrivilege> childPrivilages;
+	
 	
 	@JsonIgnore
 	@OneToMany(mappedBy = "role")
