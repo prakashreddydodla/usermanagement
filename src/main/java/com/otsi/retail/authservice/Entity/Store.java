1ba4bce48f447535935c27d1aeddf2008ed6c801
vasavi yakkali <vasavi.yakkali@otsi.co.in>
2022-09-22 14:50:37 +0530

    added indexes

--- src/main/java/com/otsi/retail/authservice/Entity/Store.java
+++ src/main/java/com/otsi/retail/authservice/Entity/Store.java
@@ -3,21 +3,26 @@ package com.otsi.retail.authservice.Entity;
 import java.util.List;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Index;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
+import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.Data;
 
 @Entity
+@Table(name = "store", indexes = { @Index(name = "clientid__index", columnList = "client_id"),
+		@Index(name = "clientid_isactive_index", columnList = "client_id, is_active") })
 @Data
 public class Store extends BaseEntity {
 
@@ -41,6 +46,7 @@ public class Store extends BaseEntity {
 
 	private String phoneNumber;
 
+	@Column(name = "is_active")
 	private Boolean isActive = Boolean.FALSE;
 
 	@ManyToOne
