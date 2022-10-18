c40dd4aa59b65f05280512c0d84bafb076e6687d
vengalarao <chandra.vengalarao>
2022-10-18 15:37:06 +0530

    Added API GetClientById

--- src/main/java/com/otsi/retail/authservice/utils/EndpointConstants.java
+++ src/main/java/com/otsi/retail/authservice/utils/EndpointConstants.java
@@ -70,6 +70,7 @@ public class EndpointConstants {
 	public static final String GET_CLIENT_STORES = "/client/stores";
 	public static final String GET_USER_STORESBYID = "/user/stores";
 	public static final String GET_STORE = "/getStore"; 
+	public static final String GET_STORES_BY_CLIENT_ID = "/getStoresByClientId";
 
 	public static final String ASSIGN_STORES_TO_DOMIAN = "/assignStoresToDomain";
 	public static final String GET_STORES_WITH_FILTER = "/getStoresWithFilter";
